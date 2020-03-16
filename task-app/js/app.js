a339781714d8866e5b4db5889eebab16a304cf12
Isaí Hernández <iasiher13@gmail.com>
2020-03-16 13:39:49 -0600

    Update and delete task

--- task-app/js/app.js
+++ task-app/js/app.js
@@ -1,12 +1,13 @@
 $(function(){
 
+    let edit =  false;
     fetchTasks();
     
     let formSearch = document.querySelector('#search'),
         containerTasks = document.querySelector('#container'),
         tasksResults = document.querySelector('#tasks-result'),
-        taskForm = document.querySelector('#task-form'),
-        tasksContent = document.querySelector('#tasks');
+        taskForm = document.querySelector('#task-form');
+        
 
         tasksResults.classList.add('d-none');
 
@@ -35,9 +36,11 @@ $(function(){
         event.preventDefault();
         const postData = {
             name: document.querySelector('#name').value,
-            description: document.querySelector('#description').value
+            description: document.querySelector('#description').value,
+            id: document.querySelector('#taskId').value
          };
-         $.post('task-add.php', postData, function(response){
+         let url = edit === false ? 'task-add.php': 'task-edit.php';
+         $.post(url, postData, function(response){
              fetchTasks();
              taskForm.reset();
          });
@@ -51,15 +54,45 @@ $(function(){
                 let allTasks = JSON.parse(response), template ='';
                 [].forEach.call(allTasks, function(task){
                     template += `
-                    <tr>
+                    <tr taskId='${task.id}'>
                     <td>${task.id}</td>
-                    <td>${task.name}</td>
+                    <td>
+                        <a href="#" class="task-item">${task.name}</a>
+                    </td>
                     <td>${task.description}</td>
+                    <td>
+                        <button class="task-delete btn btn-danger">
+                            Delete
+                        </button>
+                    </td>
                     </tr>
                     `
                 });
-                tasksContent.innerHTML += template;
+                $('#tasks').html(template);
             }
         });
-    }
+    };
+
+    $(document).on('click', '.task-delete', function(){
+        if(confirm('Are you sure you wantto delte it ?')){
+            let element = $(this)[0].parentElement.parentElement,
+            id = $(element).attr('taskId');
+            $.post('task-delete.php', {id}, function(response){
+                console.log(response);
+                fetchTasks();
+            });
+        }
+    });
+
+    $(document).on('click', '.task-item', function(){
+        let element = $(this)[0].parentElement.parentElement,
+        id =  $(element).attr('taskId');
+        $.post('task-single.php', {id}, function(response){
+            const task = JSON.parse(response);
+            $('#name').val(task.name);
+            $('#description').val(task.description);
+            $('#taskId').val(task.id);
+            edit = true;
+        });
+    });
 });
